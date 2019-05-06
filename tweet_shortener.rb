ef1a88717808eb4e9c38883615ffdec1227aebcc
Nia Bert <nbert14@gmail.com>
2019-05-06 23:42:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,18 +1,20 @@
 # Write your code here.
-def dictionary
-words = { 
-    "hello" => "hi", 
-    "to" => "2",
+def dictionary 
+  dict = {
+    "hello" => 'hi',
+    "to"=> "2",
+    "too"=> "2",
     "two" => "2",
-    "too" => "2",
-    "for" => "4",
-    "four" => "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&"
-  }
-end
+    "for"=>"4",
+    "four" => "4", 
+    "For"=>"4",
+    "Four" => "4",
+    'be' => "b",
+    'you'=> "u",
+    "at" =>"@",
+    "and" => "&",
+    }
+end 
   
 def word_substituter(tweet)
   dict = dictionary
