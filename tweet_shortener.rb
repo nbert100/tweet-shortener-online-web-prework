a60c74d4dd4c518aa6c14578b3afe2a343f37384
Nia Bert <nbert14@gmail.com>
2019-05-06 22:54:44 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,18 @@
 # Write your code here.
+words = { 
+    "hello" => "hi", 
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  
 def word_substituter(word)
-  words = { "hello" =>
+  
+    
 end
